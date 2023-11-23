2f8a321ce3b710f7820d88b7830a08a042126cdc
sidiousvic <sidiousvic@gmail.com>
2023-11-23 23:25:22 +0900

    feat: lift curtain on DOMContentLoaded for posts

--- @/liftCurtain.mjs
+++ @/liftCurtain.mjs
@@ -1,4 +1,10 @@
-addEventListener("load", () => {
+const whenToLift = document.querySelector(".post")
+  ? "DOMContentLoaded"
+  : "load";
+
+console.log(`whenToLift: ${whenToLift}`);
+
+addEventListener(whenToLift, () => {
   const curtainElement = document.getElementById("curtain");
   curtainElement.style.opacity = `0`;
 });
