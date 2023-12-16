e0e293f322d3fb3a6ddce53ec2aa1f3a427c4a3a
sidiousvic <sidiousvic@gmail.com>
2023-12-16 21:37:36 +0900

    refactor: page info script

---
+++ @/programs/togglePageInfo.mjs
@@ -0,0 +1,13 @@
+const pageInfoOverlay = document.querySelector(".page-info-overlay");
+const pageInfoButton = document.querySelector(".page-info-button");
+
+const hideOverlay = () => (pageInfoOverlay.style.display = "none");
+const showOverlay = () => (pageInfoOverlay.style.display = "flex");
+
+addEventListener(
+  "DOMContentLoaded",
+  () => (
+    pageInfoOverlay.addEventListener("click", hideOverlay),
+    pageInfoButton.addEventListener("click", showOverlay)
+  )
+);
