175b179c8d5a36af4ab053d585b32b480716a4da
sidiousvic <sidiousvic@gmail.com>
2023-12-28 22:26:16 +0900

    feat: navigate to meta with m keypress

--- _includes/ts/keyboardnav.ts
+++ _includes/ts/keyboardnav.ts
@@ -7,4 +7,5 @@ import { getElement } from "./domutils";
   (e: KeyboardEvent) => e.key === "s" && location.assign("/sitemap"),
   (e: KeyboardEvent) => e.key === "v" && location.assign("/about"),
   (e: KeyboardEvent) => e.key === "p" && location.assign("/projects"),
+  (e: KeyboardEvent) => e.key === "m" && location.assign("/meta"),
 ].map((f) => window.addEventListener("keydown", f));
