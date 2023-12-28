2185b37991d5d75f7131c3e3f10ce53b02038ffa
sidiousvic <sidiousvic@gmail.com>
2023-12-28 23:56:45 +0900

    refactor: lastupdated.ts

--- _includes/ts/lastupdated.ts
+++ _includes/ts/lastupdated.ts
@@ -1,24 +1,37 @@
 import { getElementById } from "./domutils";
+import { inject, isMobile, mutate, pipe } from "./utils";
 
-const REPO_URL = "https://api.github.com/repos/sidiousvic/sidious.pizza";
+type Config = {
+  repoUrl: "https://api.github.com/repos/sidiousvic/sidious.pizza";
+  dateTimeFormat: Intl.DateTimeFormat;
+};
 
-const dateTimeFormat = new Intl.DateTimeFormat("en-US", {
-  weekday: "long",
-  year: "numeric",
-  month: "short",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  hour12: true,
-});
+const config: Config = {
+  repoUrl: "https://api.github.com/repos/sidiousvic/sidious.pizza",
+  dateTimeFormat: new Intl.DateTimeFormat("en-US", {
+    weekday: isMobile(navigator.userAgent) ? "short" : "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: isMobile(navigator.userAgent) ? false : true,
+  }),
+};
 
-addEventListener("DOMContentLoaded", () =>
-  fetch(REPO_URL)
-    .then((r) => r.json())
-    .then(
-      ({ pushed_at }) =>
-        ((
-          getElementById("last-updated-datetime") as HTMLSpanElement
-        ).innerText = dateTimeFormat.format(new Date(pushed_at)))
-    )
+const fetchRepo = async (z: Config) => fetch(z.repoUrl).then((r) => r.json());
+
+const updateLastUpdated = pipe(
+  inject(config),
+  fetchRepo,
+  mutate(
+    async (promise: Promise<{ pushed_at: string }>) =>
+      ((
+        getElementById("last-updated-datetime") as HTMLSpanElement
+      ).innerText = `${config.dateTimeFormat.format(
+        new Date((await promise).pushed_at)
+      )} ${isMobile(navigator.userAgent) ? " Tokyo" : " Asia/Tokyo"}`)
+  )
 );
+
+addEventListener("DOMContentLoaded", updateLastUpdated);
