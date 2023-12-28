51a8df87e7226c86577f953d619484276683ce7d
sidiousvic <sidiousvic@gmail.com>
2023-12-28 11:12:24 +0900

    feat: add expires

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import Server from "lume/core/server.ts";
 import notFound from "lume/middlewares/not_found.ts";
+import expires from "lume/middlewares/expires.ts";
 
 const server = new Server({
   port: 8000,
@@ -12,6 +13,7 @@ server.use(
     page404: "404.html",
     directoryIndex: true,
   }),
+  expires(),
 );
 
 server.start();
