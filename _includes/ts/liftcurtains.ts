d67daf19c91e05ea99c37adc0e1b12e3c78636a8
sidiousvic <sidiousvic@gmail.com>
2024-01-10 10:05:30 +0900

    fix: formatting

--- _includes/ts/liftcurtains.ts
+++ _includes/ts/liftcurtains.ts
@@ -7,11 +7,10 @@ const config: Config = { curtainId: "curtain" };
 
 type State = Config & Event;
 
-const setCurtainOpacityToZero = (z: State) => (
-  Try(document.getElementById(z.curtainId))(`ID ${z.curtainId} not found.`)
-    .style
-    .opacity = `0`
-);
+const setCurtainOpacityToZero = (z: State) =>
+  (Try(document.getElementById(z.curtainId))(
+    `ID ${z.curtainId} not found.`
+  ).style.opacity = `0`);
 
 const liftCurtain = pipe(inject(config), mutate(setCurtainOpacityToZero));
 
