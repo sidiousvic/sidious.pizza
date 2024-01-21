b5c94d77e0c6a5daff588f216156b86d8a1db940
sidiousvic <sidiousvic@gmail.com>
2024-01-21 21:29:57 +0900

    feat: navigate to config with k

--- _includes/ts/keyboardnav.ts
+++ _includes/ts/keyboardnav.ts
@@ -1,6 +1,6 @@
 [
   (e: KeyboardEvent) => e.key === "h" && location.assign("/"),
-  (e: KeyboardEvent) => e.key === "c" && location.assign("/config"),
+  (e: KeyboardEvent) => e.key === "k" && location.assign("/config"),
   (e: KeyboardEvent) => e.key === "w" && location.assign("/weblog"),
   (e: KeyboardEvent) => e.key === "s" && location.assign("/sitemap"),
   (e: KeyboardEvent) => e.key === "v" && location.assign("/about"),
