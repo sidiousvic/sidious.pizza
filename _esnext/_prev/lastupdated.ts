2540b118f095e6d03a770a56d5a3464d8e7efcec
sidiousvic <sidiousvic@gmail.com>
2025-05-04 14:23:27 +0900

    feat: fallback last updated in dev mode
    Also introduces dev mode

--- _esnext/_prev/lastupdated.ts
+++ _esnext/_prev/lastupdated.ts
@@ -5,6 +5,7 @@ import { error, inject, mutate, pipe } from "./utils";
 type Config = {
   repoUrl: "https://api.github.com/repos/sidiousvic/sidious.pizza";
   dateTimeFormat: Intl.DateTimeFormat;
+  isDevMode: boolean;
 };
 
 const config: Config = {
@@ -18,24 +19,29 @@ const config: Config = {
     minute: "2-digit",
     hour12: false,
   }),
+  isDevMode: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1",
 };
 
 const updateLastUpdated = pipe(
   inject(config),
-  async (z: Config) =>
-    fetch(z.repoUrl).then(async (r) => {
+  async (z: Config) => {
+    if (z.isDevMode) {
+      return { pushed_at: new Date().toISOString() };
+    }
+    return fetch(z.repoUrl).then(async (r) => {
       const data = await r.json();
       if (JSON.stringify(data).includes("rate limit"))
         error(GitHubRateLimitExceededError);
       return data;
-    }),
+    });
+  },
   mutate(
     async (promise: Promise<{ pushed_at: string }>) =>
-      (getElementById(
-        "last-updated-datetime"
-      ).innerText = `${config.dateTimeFormat.format(
-        new Date((await promise)?.pushed_at || Date.now())
-      )} Asia/Pacific (Tokyo)`)
+    (getElementById(
+      "last-updated-datetime"
+    ).innerText = `${config.dateTimeFormat.format(
+      new Date((await promise)?.pushed_at || Date.now())
+    )} Asia/Pacific (Tokyo)`)
   )
 );
 
