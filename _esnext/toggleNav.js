d419f9d2cfb5bb32630cacd882ba3f9df89aed9a
sidiousvic <sidiousvic@gmail.com>
2025-05-05 12:30:33 +0900

    fix: full menu on mobile

--- _esnext/toggleNav.js
+++ _esnext/toggleNav.js
@@ -1,1 +1,21 @@
-(()=>{function n(){let t=document.getElementById("nav-toggle"),e=document.getElementById("nav-links");t&&e&&t.addEventListener("click",()=>{e.classList.toggle("active")})}n();})();
+(() => {
+    function initNav() {
+        const navToggle = document.getElementById("nav-toggle");
+        const navLinks = document.getElementById("nav-links");
+        const closeMenu = document.querySelector(".close-menu");
+
+        if (navToggle && navLinks) {
+            navToggle.addEventListener("click", () => {
+                navLinks.classList.toggle("active");
+            });
+        }
+
+        if (closeMenu && navLinks) {
+            closeMenu.addEventListener("click", () => {
+                navLinks.classList.remove("active");
+            });
+        }
+    }
+
+    initNav();
+})();
